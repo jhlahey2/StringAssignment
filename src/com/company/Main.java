af5f782fb5d7f6364801ca2878f47ab35776c0ab
jhlahey2 <jhlahey@protonmail.com>
2018-10-04 09:19:16 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Assignment
+ * String assignment
+ * Write a Java program that assigns your name to a variable and prints the variable to the console.
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String myName = "Jack";
+        System.out.println("My name is " + myName);
+    }
+}
